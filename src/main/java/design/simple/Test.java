c90218d3e69513ee4c57282023bd10fc9b5edb6b
hzh199094 <hzh199094@163.com>
2016-12-09 18:54:33 +0800

    1

--- src/main/java/design/simple/Test.java
+++ src/main/java/design/simple/Test.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 
 import design.simple.entiy.Nums;
 
-//输入2个数字和一个运算符，就结果：使用到了简单工厂模式，对象的创建和使用分离
-//有一个工厂，专门用来创建对象，不同的操作符对应不同的
+/**
+      输入2个数字和一个运算符，就结果：使用到了简单工厂模式，对象的创建和使用分离
+      有一个工厂，专门用来创建对象，不同的操作符对应不同的工厂子类；工厂方法根据传入
+      的操作符不同创建不同的类
+ */
 public class Test {
 
 	public static void main(String[] args) {
